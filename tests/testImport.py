5872af6c5e9371a13f34f12d31a753b9ee204f3e
PeterC <petercalifano.gs@gmail.com>
2024-09-17 15:56:02 +0200

    Test import completed

--- tests/testImport.py
+++ tests/testImport.py
@@ -1,1 +1,19 @@
-import    # Custom torch tools
+from pyTorchAutoForge import utils
+from pyTorchAutoForge import api
+from pyTorchAutoForge import optimization
+
+from pyTorchAutoForge.optimization import ModelTrainingManager
+from torch.utils import data
+
+if __name__ == '__main__':
+    print("Imported modules successfully.")
+    print(f"utils: {utils}")
+    print(f"api: {api}")
+    print(f"optimization: {optimization}")
+
+    print(f"data from pytorch: {data}")
+
+
+
+
+ 
