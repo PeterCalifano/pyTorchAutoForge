55a5a38717386e52b8a32dcd960243f4d519e3a7
PeterC <petercalifano.gs@gmail.com>
2024-09-19 08:50:35 +0200

    Add testTorchModelMATLABwrapper class for unit testing (WIP)

---
+++ tests/matlab/testTorchModelMATLABwrapper.m
@@ -0,0 +1,20 @@
+classdef testTorchModelMATLABwrapper < matlab.unittest.TestCase
+    % TBD: how to use this class?
+    
+    methods (TestClassSetup)
+        % Shared setup for the entire test class
+    end
+
+    methods (TestMethodSetup)
+        % Setup for each test
+    end
+
+    methods (Test)
+        % Test methods
+
+        function unimplementedTest(testCase)
+            testCase.verifyFail("Unimplemented test");
+        end
+    end
+
+end
