19037f2bbabddf03a96ebd04db6c6b2febef4e81
PeterC <petercalifano.gs@gmail.com>
2024-09-19 09:34:41 +0200

    Minor changes

--- pyTorchAutoForge/setup/env_setup.py
+++ pyTorchAutoForge/setup/env_setup.py
@@ -1,1 +1,7 @@
-# TODO
+# TODO: Which operations are required to setup the package environment?
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
