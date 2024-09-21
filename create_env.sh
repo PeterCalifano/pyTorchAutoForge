a0193d8d4de395fb4aeaa5f08eaf567f8887d165
PeterC <petercalifano.gs@gmail.com>
2024-09-21 11:46:28 +0200

    Specified python version

--- create_env.sh
+++ create_env.sh
@@ -1,5 +1,5 @@
 sudo apt install python3.11 python3.11-venv # Install python3-venv
-python3 -m venv .venvTorch # Create virtual environment
+python3.11 -m venv .venvTorch # Create virtual environment
 source .venvTorch/bin/activate # Activate virtual environment
 pip install -r requirements.txt --require-virtualenv # Install dependencies
 pip install . --require-virtualenv # Install the package
