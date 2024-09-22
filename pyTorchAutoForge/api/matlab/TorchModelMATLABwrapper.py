b57ceb2fedba1e526a89e51184c43370ffa7c142
PeterC <petercalifano.gs@gmail.com>
2024-09-22 12:55:54 +0200

    Improvements to class for efficiency and robustness

--- pyTorchAutoForge/api/matlab/TorchModelMATLABwrapper.py
+++ pyTorchAutoForge/api/matlab/TorchModelMATLABwrapper.py
@@ -3,38 +3,56 @@ from pyTorchAutoForge.api.torch import LoadTorchModel
 import numpy as np
 import torch
 
+from typing import Union
+
 # %% MATLAB wrapper class for Torch models evaluation - 11-06-2024
 class TorchModelMATLABwrapper():
-    def __init__(self, trainedModelName: str, trainedModelPath: str) -> None:
+    '''Class to wrap a trained PyTorch model for evaluation in MATLAB'''
+
+    def __init__(self, trainedModelPath: str, trainedModelName: Union[str, None] = None, DEBUG_MODE: bool = False) -> None:
+        '''Constructor for TorchModelMATLABwrapper'''
+
         # Get available device
         self.device = GetDevice()
 
         # Load model state and state
-        trainedModel = LoadTorchModel(
-            None, trainedModelName, trainedModelPath, loadAsTraced=True)
-        trainedModel.eval()  # Set model in evaluation mode
-        self.trainedModel = trainedModel.to(self.device)
+        if trainedModelName is None:
+            # TODO: Implement model name extraction from path
+            raise NotImplementedError('Current version is not able to separate model name from path. Please provide model name as second argument.')
 
-    def forward(self, inputSample: np.array, numBatches: int = 1):
-        '''Forward method to perform inference for ONE sample input using trainedModel'''
-        if inputSample.dtype is not np.float32:
+        self.trainedModel = LoadTorchModel(None, trainedModelName, trainedModelPath, loadAsTraced=True).to(self.device)
+        (self.trainedModel).eval()  # Set model in evaluation mode
+
+        # Set debug mode flag
+        self.DEBUG_MODE = DEBUG_MODE
+
+    def forward(self, inputSample: Union[np.array, torch.tensor], numBatches: int = 1):
+        '''Forward method to perform inference on N sample input using loaded trainedModel. Batch size assumed as 1 if not given.'''
+
+        # Check input type and convert to torch.tensor if necessary
+        if inputSample is np.array and inputSample.dtype != np.float32:
+            Warning('Converting input to np.float32 from', inputSample.dtype)
             inputSample = np.float32(inputSample)
 
-        print('Performing formatting of input array to pass to model...')
+        elif inputSample is torch.tensor and inputSample.dtype != torch.float32:
+            Warning('Converting input to torch.float32 from', inputSample.dtype)
+            inputSample = inputSample.float()
 
-        # TODO: check the input is exactly identical to what the model receives using EvaluateModel() loading from dataset!
         # Convert numpy array into torch.tensor for model inference
-        X = torch.tensor(inputSample).reshape(numBatches, -1)
+        X = torch.tensor(inputSample).reshape(numBatches, -1) if inputSample is np.array else inputSample.reshape(numBatches, -1)
 
         # ########### DEBUG ######################:
-        print('Evaluating model using batch input: ', X)
+        if self.DEBUG_MODE:
+            print('Input sample shape: ', X.shape, 'on device: ', self.device)
+            print('Evaluating model using batch input: ', X)
         ############################################
 
         # Perform inference using model
         Y = self.trainedModel(X.to(self.device))
 
         # ########### DEBUG ######################:
-        print('Model prediction: ', Y)
+        if self.DEBUG_MODE:
+            print('Model prediction: ', Y)
         ############################################
 
-        return Y.detach().cpu().numpy()  # Move to cpu and convert to numpy
+        return Y.detach().cpu().numpy()  # Move to cpu and convert to numpy before returning
