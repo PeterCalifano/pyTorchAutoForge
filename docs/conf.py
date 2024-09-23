d2b6059efae53e0465efaf81a30c0e75ee70b341
PeterC <petercalifano.gs@gmail.com>
2024-09-23 11:08:34 +0200

    Update Sphinx configuration for autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -26,8 +26,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-
 
 # -- Options for autodoc -----------------------------------------------------
 napoleon_google_docstring = True  # Enable Google-style
