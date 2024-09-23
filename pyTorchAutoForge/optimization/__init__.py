ea9b42cd5e2e107294ef526e6603e449b3db4ecb
PeterC <petercalifano.gs@gmail.com>
2024-09-23 18:46:55 +0200

    Modified name of module

--- pyTorchAutoForge/optimization/__init__.py
+++ pyTorchAutoForge/optimization/__init__.py
@@ -1,4 +1,4 @@
 from .ModelTrainingManager import *
-from .lossFcns import *
+from .lossFunctionsClasses import *
 
 # DEVNOTE: torch has __all__ defined for all classes in files. What is its purpose?
--- pyTorchAutoForge/optimization/lossFunctionsClasses.py
+++ pyTorchAutoForge/optimization/lossFunctionsClasses.py
@@ -4,12 +4,12 @@ from typing import Union
 # %% Class to define a custom loss function for training, validation and testing - 01-06-2024
 # NOTE: Function EvalLossFcn must be implemented using Torch operations to work!
 
-class CustomLossFcn(Union[torch.nn.Module]):
+class CustomLossFcn_old(torch.nn.Module):
     '''Custom loss function based class, instantiated by specifiying a loss function (callable object) and optionally, a dictionary containing parameters required for the evaluation'''
 
     def __init__(self, EvalLossFcn: callable, lossParams: dict = None) -> None:
         '''Constructor for CustomLossFcn class'''
-        super(CustomLossFcn, self).__init__()  # Call constructor of nn.Module
+        super(CustomLossFcn_old, self).__init__()  # Call constructor of nn.Module
 
         if len((inspect.signature(EvalLossFcn)).parameters) >= 2:
             self.LossFcnObj = EvalLossFcn
@@ -29,6 +29,27 @@ class CustomLossFcn(Union[torch.nn.Module]):
         ''''Forward pass method to evaluate loss function on input and label vectors using EvalLossFcn'''
         lossBatch = self.LossFcnObj(
             predictVector, labelVector, self.lossParams)
+
+        if isinstance(lossBatch, torch.Tensor):
+            assert (lossBatch.dim() == 0)
+        elif isinstance(lossBatch, dict):
+            assert (lossBatch.get('lossValue').dim() == 0)
+        else:
+            raise ValueError(
+                'EvalLossFcn must return a scalar loss value (torch tensor) or a dictionary with a "lossValue" key')
+
+        return lossBatch
+
+
+class CustomLossFcn(torch.nn.Module):
+    '''Custom loss function based class, instantiated by specifiying a loss function (callable object) and optionally, a dictionary containing parameters required for the evaluation'''
+
+    def __init__(self) -> None:
+        super().__init__()  # Call constructor of nn.Module
+        
+    def forward(self, predictedVector, labelVector):
+        ''''Forward pass method to evaluate loss function on input and label vectors using EvalLossFcn'''
+        lossBatch = self.LossFcnObj( predictedVector, labelVector, self.lossParams)
 
         if isinstance(lossBatch, torch.Tensor):
             assert (lossBatch.dim() == 0)
