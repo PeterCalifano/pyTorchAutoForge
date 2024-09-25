8934075c1fa46b22b8c36f825f1e41a53064061f
PeterC <petercalifano.gs@gmail.com>
2024-09-25 23:07:20 +0200

    Add installation script for pytorch-autoforge submodule

---
+++ install_submodule.sh
@@ -0,0 +1,13 @@
+echo "Hello! Installation as submodule begins. This is going to install pytorch-autoforge with all the dependencies in a virtual env and build its documentation."
+echo -e "Starting in 1 second...\n\n"
+sleep 1
+sudo apt install python3.11 python3.11-venv # Install python3-venv
+cd ../../ # Go to the root directory (assuming pytorch-autoforge is in root/submodules/)
+python3.11 -m venv .venvTorch # Create virtual environment
+source .venvTorch/bin/activate # Activate virtual environment
+pip install -r requirements.txt --require-virtualenv # Install dependencies
+pip install . --require-virtualenv # Install the package
+cd submodules/pytorch_autoforge
+# Install sphinx and theme, and build the documentation
+pip install sphinx sphinx_rtd_theme --require-virtualenv
+make -C docs html
