a88c95a69af3db6748f34d3bb8780aa3dc4842b7
PeterC <petercalifano.gs@gmail.com>
2024-10-11 13:46:36 +0200

    Added imports

---
+++ tests/testTorchModel.py
@@ -0,0 +1,14 @@
+import torch 
+from pyTorchAutoForge.utils import GetDevice
+from pyTorchAutoForge.api.torch.torchModulesIO import *
+from pyTorchAutoForge.modelBuilding import torchModel
+
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
