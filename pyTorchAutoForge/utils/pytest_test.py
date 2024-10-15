63df78829c7573ce7a18556f600743954912b9e0
PeterC <petercalifano.gs@gmail.com>
2024-10-15 15:55:45 +0200

    Added bash script to run pytest. Added test functions to existing code.

--- pyTorchAutoForge/utils/pytest_test.py
+++ pyTorchAutoForge/utils/pytest_test.py
@@ -1,13 +1,36 @@
 import pytest
 
-
+# Example using simple assert --> pytest shows traceback
 def inc(x):
     return x + 1
 def test_1():
     assert inc(4) == 5
 
 def test_2():
-    assert inc(3) == 5
+    assert inc(4) == 5
+
+# Example using pytest.raises for specific exceptions
+def f():
+    raise SystemExit(1)
+
+def test_3():
+    with pytest.raises(SystemExit):
+        f()
+
+def f_2():
+    raise ExceptionGroup(
+        "Group message",
+        [
+            RuntimeError(),
+        ],
+    )
+
+def test_exception_in_group():
+    with pytest.raises(ExceptionGroup) as excinfo:
+        f_2()
+    assert excinfo.group_contains(RuntimeError)
+    assert not excinfo.group_contains(TypeError)
+
 
 # DEVNOTE: pytest does NOT run test scripts as main. The test asserts must be reachable outside other functions.
 # All functions with test* in their name are executed as unit test.
