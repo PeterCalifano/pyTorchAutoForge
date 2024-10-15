30de646f84a558ed4f766b81c318d8c934881df6
PeterC <petercalifano.gs@gmail.com>
2024-10-15 15:38:34 +0200

    Completed script to show pytest usage

--- pyTorchAutoForge/utils/pytest_test.py
+++ pyTorchAutoForge/utils/pytest_test.py
@@ -3,16 +3,22 @@ import pytest
 
 def inc(x):
     return x + 1
-def test_answer():
+def test_1():
+    assert inc(4) == 5
+
+def test_2():
     assert inc(3) == 5
 
-
+# DEVNOTE: pytest does NOT run test scripts as main. The test asserts must be reachable outside other functions.
+# All functions with test* in their name are executed as unit test.
 def main():
     # Define example unit test
     def inc(x):
         return x + 1
     def test_answer():
         assert inc(3) == 5
+
+    test_answer(4) # This never runs
     # Run the test (call from shell)
     pytest.main(['-x', __file__])
 
