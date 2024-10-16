cfe571627b26a43f404c66aed3365069d4b9797d
PeterC <petercalifano.gs@gmail.com>
2024-10-16 09:49:17 +0200

    Update to use script from shell outside VSC (my PCs only)

--- start_mlflow_server.sh
+++ start_mlflow_server.sh
@@ -1,3 +1,5 @@
 # NOTE: this script only works on peterc-desktopMSI. Server is started using the same uri as pointed by MLFLOW_TRACKING_URI environment variable, therefore works as local database automatically.
+
+source /home/peterc/devDir/pyTorchAutoForge/.venvTorch/bin/activate
 mlflow server --workers 2 --host 0.0.0.0 --port 8080 --backend-store-uri ${MLFLOW_TRACKING_URI} --default-artifact-root ${MLFLOW_ARTIFACTS_URI} 
 #mlflow server --workers 2 --host 0.0.0.0 --port 8080 --backend-store-uri sqlite:///./localdb --default-artifact-root file:///localdb/artifacts
