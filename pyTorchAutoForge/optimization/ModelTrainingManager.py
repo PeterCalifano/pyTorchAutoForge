eedade89a55e606c31f1f3139630934df37cd4e9
PeterC <petercalifano.gs@gmail.com>
2024-11-13 10:49:36 +0100

    Adde unit test; added AdamW support for new definition; added enumeration class to select optimizer type; added makedirs to create checkpoint dir if non existent

--- pyTorchAutoForge/optimization/ModelTrainingManager.py
+++ pyTorchAutoForge/optimization/ModelTrainingManager.py
@@ -175,10 +175,16 @@ class ModelTrainingManagerConfig():
         return [f.name for f in fields(self)]
 
 
+# TODO: define enum class for optimizers selection if not provided as instance
+class enumOptimizerType(Enum):
+    SGD = 0
+    ADAM = 1
+    ADAMW = 2
+
 # %% ModelTrainingManager class - 24-07-2024
 class ModelTrainingManager(ModelTrainingManagerConfig):
     def __init__(self, model: Union[nn.Module], lossFcn: Union[nn.Module, CustomLossFcn], config: Union[ModelTrainingManagerConfig, dict, str],
-                 optimizer: Union[optim.Optimizer, int, None] = None, dataLoaderIndex: Optional[DataloaderIndex] = None, paramsToLogDict: dict = None) -> None:
+                 optimizer: Union[optim.Optimizer, enumOptimizerType, None] = None, dataLoaderIndex: Optional[DataloaderIndex] = None, paramsToLogDict: dict = None) -> None:
         """
         Initializes the ModelTrainingManager class.
 
@@ -206,6 +212,11 @@ class ModelTrainingManager(ModelTrainingManagerConfig):
             # Call init of parent class for shallow copy
             super().__init__(**config.getConfigDict())
 
+        # Check that checkpoingDir exists, if not create it
+        if not os.path.isdir(self.checkpointDir):
+            Warning(f"Checkpoint directory {self.checkpointDir} does not exist. Creating it...")
+            os.makedirs(self.checkpointDir)
+        
         # Initialize ModelTrainingManager-specific attributes
         self.model = (model).to(self.device)
         self.bestModel = None
@@ -244,20 +255,13 @@ class ModelTrainingManager(ModelTrainingManagerConfig):
         if optimizer is not None:
             if isinstance(optimizer, optim.Optimizer):
                 self.reinstantiate_optimizer_()
-            elif isinstance(optimizer, int) or issubclass(optimizer, optim.Optimizer):
+            elif isinstance(optimizer, enumOptimizerType) or issubclass(optimizer, optim.Optimizer):
                 self.define_optimizer_(optimizer)
-            else:
-                raise ValueError(
-                    'Optimizer must be either an instance of torch.optim.Optimizer or an integer representing the optimizer type.')
         else:
-            if isinstance(self.optimizer, optim.Optimizer):
-                # Redefine optimizer class as workaround for weird python behavior (no update applied to model)
-                self.reinstantiate_optimizer_()
-            else:
-                raise ValueError(
-                    'Optimizer must be specified either in the ModelTrainingManagerConfig as torch.optim.Optimizer instance or as an argument in __init__ of this class!')
+            raise ValueError('Optimizer must be specified either in the ModelTrainingManagerConfig as torch.optim.Optimizer instance or as an argument in __init__ of this class!')
 
-    def define_optimizer_(self, optimizer: Union[optim.Optimizer, int]) -> None:
+
+    def define_optimizer_(self, optimizer: Union[optim.Optimizer, enumOptimizerType]) -> None:
         """
         Define and set the optimizer for the model training.
 
@@ -270,12 +274,22 @@ class ModelTrainingManager(ModelTrainingManagerConfig):
         Raises:
         ValueError: If the optimizer ID is not recognized (i.e., not 0 or 1).
         """
-        if optimizer == 0 or optimizer == torch.optim.SGD:
+        fused_ = True if "cuda" in self.device else False
+
+        if optimizer == enumOptimizerType.SGD or optimizer == torch.optim.SGD:
             self.optimizer = torch.optim.SGD(
                 self.model.parameters(), lr=self.initial_lr, momentum=self.momentumValue)
-        elif optimizer == 1 or optimizer == torch.optim.Adam:
+            
+        elif optimizer == enumOptimizerType.ADAM or optimizer == torch.optim.Adam:
             self.optimizer = torch.optim.Adam(
-                self.model.parameters(), lr=self.initial_lr)
+                self.model.parameters(), lr=self.initial_lr, fused=fused_)
+            
+        elif optimizer == enumOptimizerType.ADAMW or optimizer == torch.optim.AdamW:
+            self.optimizer = torch.optim.AdamW(
+                self.model.parameters(), lr=self.initial_lr, fused=fused_)
+        else: 
+            raise ValueError('Optimizer not recognized. Please provide a valid optimizer type or ID from enumOptimizerType enumeration class.')
+        
 
     def reinstantiate_optimizer_(self):
         """
@@ -631,7 +645,6 @@ class ModelTrainingManager(ModelTrainingManagerConfig):
                         if modelSaveName is not None:
 
                             # Get file name with modelSaveName as prefix
-                             # Get the file name with modelSaveName as prefix
                             checkpoint_files = glob.glob( f"{os.path.join(self.checkpointDir, self.modelName)}_epoch*" )
 
                             if checkpoint_files:
@@ -1037,7 +1050,7 @@ def FreezeModel(model: nn.Module) -> nn.Module:
 
 ####################################################################################################
 
-# LEGACY FUNCTIONS - 18/09/2024
+# LEGACY (no longer maintained) FUNCTIONS - 18/09/2024
 # %% Function to perform one step of training of a model using dataset and specified loss function - 04-05-2024
 # Updated by PC 04-06-2024
 
@@ -1257,8 +1270,9 @@ def ValidateModel(dataloader: DataLoader, model: nn.Module, lossFcn: nn.Module, 
     # TODO: add command for Tensorboard here
 
 
-# %% TRAINING and VALIDATION template function - 04-06-2024
+# %% TRAINING and VALIDATION template function (LEGACY, no longer maintained) - 04-06-2024
 def TrainAndValidateModel(dataloaderIndex: DataloaderIndex, model: nn.Module, lossFcn: nn.Module, optimizer, config: dict = {}):
+
     '''Function to train and validate a model using specified dataloaders and loss function'''
     # NOTE: is the default dictionary considered as "single" object or does python perform a merge of the fields?
 
@@ -1442,7 +1456,7 @@ def TrainAndValidateModel(dataloaderIndex: DataloaderIndex, model: nn.Module, lo
                                exampleInput=exampleInput, targetDevice=device)
                 swa_model.train()
 
-        # %% MODEL PREDICTION EXAMPLES
+        # MODEL PREDICTION EXAMPLES
         examplePrediction, exampleLosses, inputSampleTensor, labelsSampleTensor = EvaluateModel(
             validationDataset, model, lossFcn, device, 20)
 
@@ -1504,7 +1518,6 @@ def TrainAndValidateModel(dataloaderIndex: DataloaderIndex, model: nn.Module, lo
 
 # %% Model evaluation function on a random number of samples from dataset - 06-06-2024
 # Possible way to solve the issue of having different cost function terms for training and validation --> add setTrain and setEval methods to switch between the two
-
 
 def EvaluateModel(dataloader: DataLoader, model: nn.Module, lossFcn: nn.Module, device=GetDevice(), numOfSamples: int = 10,
                   inputSample: torch.tensor = None, labelsSample: torch.tensor = None) -> np.array:
@@ -1586,7 +1599,117 @@ def EvaluateModel(dataloader: DataLoader, model: nn.Module, lossFcn: nn.Module, 
 
 # %% TEST CODE
 def ModelTrainingManager_test_():
-    pass
+
+    from torchvision import models
+    from torchvision import transforms
+    from torchvision import datasets  # Import vision default datasets from torchvision
+
+    def DefineModel(trial:optuna.Trial = None):
+
+        model = models.resnet101(weights=False)  # Load pretrained weights
+
+        # Therefore, let's modify the last layer! (Named fc)
+        if trial is not None:
+            numInputFeatures = trial.suggest_int('numInputFeatures', 2, 1000)
+        else:
+            numInputFeatures = model.fc.in_features  # Same as what the model uses
+
+        numOutClasses = 10  # Selected by user
+        model.fc = (nn.Linear(in_features=numInputFeatures,
+                            out_features=numOutClasses, bias=True))  # 100 classes in our case
+        
+        return model
+
+    def DefineDataloaders(trial:optuna.Trial = None):
+        # Load CIFAR10 dataset from torchvision
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(
+            (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])  # Apply normalization
+
+        # Get dataset objects
+        train_dataset = datasets.CIFAR10(
+            root='./data', train=True, download=True, transform=transform)
+        validation_dataset = datasets.CIFAR10(
+            root='./data', train=False, download=True, transform=transform)
+
+        # Define dataloaders
+        if trial is not None:
+            batch_size = trial.suggest_int('batch_size', 32, 512)
+            train_loader = DataLoader(
+                train_dataset, batch_size=batch_size, shuffle=True)
+        else:
+            batch_size = 128
+            train_loader = DataLoader(
+                train_dataset, batch_size=batch_size, shuffle=True)
+
+        validation_loader = DataLoader(
+            validation_dataset, batch_size=batch_size, shuffle=False, drop_last=False)
+        
+        return train_loader, validation_loader
+
+    def DefineOptimStrategy(trial:optuna.Trial = None):
+        if trial is not None:
+            initial_lr = trial.suggest_loguniform('initial_lr', 1E-5, 1E-1)
+        
+        else:
+            initial_lr = 1E-3
+        
+        lossFcn = nn.CrossEntropyLoss()
+
+        return lossFcn, initial_lr
+
+    # Set mlflow experiment
+    mlflow.set_experiment('ImageClassificationCIFAR10_Example')
+
+    # Get model backbone from torchvision
+    # All models in torchvision.models for classification are trained on ImageNet, thus have 1000 classes as output
+    model = DefineModel()
+    device = GetDevice()
+    model.to(device)
+    print(model)  # Check last layer
+
+    # Define loss function and optimizer
+    lossFcn, initial_lr = DefineOptimStrategy()
+    numOfEpochs = 20
+
+    fused = True if device == "cuda:0" else False
+    #optimizer = torch.optim.Adam(
+    #    model.parameters(), lr=initial_lr, fused=fused)
+
+    optimizer = torch.optim.SGD(
+        model.parameters(), lr=initial_lr)
+    
+    # Define dataloader index for training
+    train_loader, validation_loader = DefineDataloaders()  # With defaul batch size
+    dataloaderIndex = DataloaderIndex(train_loader, validation_loader)
+
+    # CHECK: versus TrainAndValidateModel
+    # model2 = copy.deepcopy(model).to(device)
+    # optimizer2 = torch.optim.Adam(model2.parameters(), lr=initial_lr, fused=fused)
+    # for epoch in range(numOfEpochs):
+    #    print(f"Epoch TEST: {epoch}/{numOfEpochs-1}")
+    #    TrainModel(dataloaderIndex.getTrainLoader(), model2, lossFcn, optimizer2, 0)
+    #    ValidateModel(dataloaderIndex.getValidationLoader(), model2, lossFcn)
+    # Define model training manager config  (dataclass init)
+    trainerConfig = ModelTrainingManagerConfig(tasktype=TaskType.CLASSIFICATION,
+                                               initial_lr=initial_lr, lr_scheduler=None,
+                                               num_of_epochs=numOfEpochs, optimizer=optimizer,
+                                               batch_size=train_loader.batch_size)
+
+    # DEVNOTE: TODO, add check on optimizer from ModelTrainingManagerConfig. It must be of optimizer type
+    print("\nModelTrainingManagerConfig instance:", trainerConfig)
+    print("\nDict of ModelTrainingManagerConfig instance:",
+          trainerConfig.getConfigDict())
+
+    # Define model training manager instance
+    trainer = ModelTrainingManager(
+        model=model, lossFcn=lossFcn, config=trainerConfig)
+    print("\nModelTrainingManager instance:", trainer)
+
+    # Set dataloaders for training and validation
+    trainer.setDataloaders(dataloaderIndex)
+
+    # Perform training and validation of model
+    trainer.trainAndValidate()
 
 if __name__ == '__main__':
     ModelTrainingManager_test_()
