25341e3086e42dc1c5e31e122439cf3a2947a49a
PeterC <petercalifano.gs@gmail.com>
2025-02-16 22:12:34 +0100

    Change tensorrt version for compatibility with torch-tensorrt

--- pyTorchAutoForge/.experimental/tutorials/tensorrt_onnx/install_tensorrt.sh
+++ pyTorchAutoForge/.experimental/tutorials/tensorrt_onnx/install_tensorrt.sh
@@ -10,7 +10,9 @@ else
 fi
 
 # Get tar file
-wget https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.8.0/tars/TensorRT-10.8.0.43.Linux.x86_64-gnu.cuda-12.8.tar.gz
+#wget https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.8.0/tars/TensorRT-10.8.0.43.Linux.#x86_64-gnu.cuda-12.8.tar.gz
+
+wget https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.7.0/tars/TensorRT-10.7.0.23.Linux.x86_64-gnu.cuda-12.6.tar.gz
 
 # Untar
 workdir=$(pwd)
@@ -18,18 +20,29 @@ workdir=$(pwd)
 tar -xzvf TensorR*
 
 # Move and rename
-sudo mv TensorRT-10.8.0.43.Linux.x86_64-gnu.cuda-12.8/ /usr/local/
+# tensorrt10.8 for cuda 12.8
+#sudo mv TensorRT-10.8.0.43.Linux.x86_64-gnu.cuda-12.8/ /usr/local/
+#cd /usr/local/
+#sudo mv TensorRT-10.8.0.43.Linux.x86_64-gnu.cuda-12.8/ tensorrt10.8-cuda12.8/
+#cd tensorrt10.8-cuda12.8/
+#mv TensorRT-10.8.0.43/* .
+#rm -r TensorRT-10.8.0.43
+
+# tensorrt10.7 for cuda 12.6
 cd /usr/local/
-sudo mv TensorRT-10.8.0.43.Linux.x86_64-gnu.cuda-12.8/ tensorrt10.8-cuda12.8/
-cd tensorrt10.8-cuda12.8/
-mv TensorRT-10.8.0.43/* .
-rm -r TensorRT-10.8.0.43
+sudo mv TensorRT-10.7.0.23.Linux.x86_64-gnu.cuda-12.6/ tensorrt10.7-cuda12.6/
+cd tensorrt10.7-cuda12.6/
+mv TensorRT-10.7.0.23/* .
+rm -r TensorRT-10.7.0.23
 
 # Add to environment variables
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/tensorrt10.8-cuda12.8/lib
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/tensorrt10.8-cuda12.8/lib
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/tensorrt10.7-cuda12.6/lib
 
 # Add export to bashrc
-echo "export LD_LIBRARY_PATH=/usr/local/tensorrt10.8-cuda12.8/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/tensorrt10.7-cuda12.6/lib" >> ~/.bashrc
+#echo "export LD_LIBRARY_PATH=/usr/local/tensorrt10.8-cuda12.8/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
 
 # Install the python wheels for python 3.11
 cd python
