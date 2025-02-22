ea70c43ae5678abd2d9978283074afe4e23ae19f
PeterC <petercalifano.gs@gmail.com>
2025-02-22 17:34:23 +0100

    Add example code for use of onnx and tensorrt engine

--- pyTorchAutoForge/.experimental/tensorrt_onnx/get_sample_models.sh
+++ pyTorchAutoForge/.experimental/tensorrt_onnx/get_sample_models.sh
@@ -3,3 +3,13 @@ wget https://developer.download.nvidia.com/devblogs/speeding-up-unet.7z
 
 wget https://s3.amazonaws.com/onnx-model-zoo/resnet/resnet50v2/resnet50v2.tar.gz
 tar xvf resnet50v2.tar.gz # unpack model data into resnet50v2 folder
+
+rm -r speeding-up-unet.7z
+rm -r resnet50v2.tar.gz
+
+# Add to gitignore the folders
+echo "resnet50v2/" >> .gitignore
+echo "speeding-up-unet/" >> .gitignore
+
+
+
