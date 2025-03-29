735ed8875dc4666d21b385b9cf246eae08cbda1a
PeterC <petercalifano.gs@gmail.com>
2025-03-29 20:15:19 +0100

    Update Sphinx doc configuration

--- doc/conf.py
+++ doc/conf.py
@@ -15,12 +15,21 @@ release: str = get_version("pyTorchAutoForge")
 project = 'pyTorchAutoForge'
 copyright = '2025, Pietro Califano'
 author = 'Pietro Califano'
+email = 'petercalifano.gs@gmail.com'
 version: str = ".".join(release.split('.')[:3])  # Major.Minor.Patch
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_rtd_theme']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.githubpages",
+    'sphinx_rtd_theme'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -34,7 +43,7 @@ html_theme_options = {
     #'analytics_id': 'G-XXXXXXXXXX',  # Provided by Google in your dashboard
     #'analytics_anonymize_ip': False,
     'logo_only': False,
-    'display_version': True,
+    #'display_version': True,
     #'prev_next_buttons_location': 'bottom',
     #'style_external_links': False,
     #'vcs_pageview_mode': '',
@@ -57,9 +66,17 @@ def setup(app):
     app.add_css_file('custom.css')
 
 
-# -- Options for autodoc -----------------------------------------------------
+# -- Options for autodoc extensions -----------------------------------------------------
 napoleon_google_docstring = True  # Enable Google-style
 napoleon_numpy_docstring = True   # Enable NumPy-style
 napoleon_include_init_with_doc = False  # Don't include __init__ docstring
 napoleon_use_param = True         # Use :param: for function params
 napoleon_use_rtype = True         # Use :rtype: for return type
+
+#autodoc_member_order = 'bysource'
+autosummary_generate = True  # Generate .rst files for all modules
+
+# Interspinx mapping for cross-referencing
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
+
+
