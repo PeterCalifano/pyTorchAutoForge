e1defa68b0756f449615ea4f6dd50d5df54d3ed6
PeterC <petercalifano.gs@gmail.com>
2025-03-29 12:03:05 +0100

    [MAJOR] Rework model mutator class to enable extensions (compatibility-breaking)

--- pyTorchAutoForge/modelBuilding/ModelMutatorBNtoGN.py
+++ pyTorchAutoForge/modelBuilding/ModelMutatorBNtoGN.py
@@ -1,34 +1,79 @@
 from torch import nn
+from enum import Enum
 
-class ModelMutatorBNtoGN():
-    def __init__(self, model:nn.Module, numOfGroups: int = 32) -> None:
+class EnumMutations(Enum):
+    BNtoGN = 0
+    #GNtoBN = 1
+
+# TODO upgrade mutator to allow replacements enumerated in EnumMutations
+class ModelMutator():
+    def __init__(self, model: nn.Module, numOfGroups: int = 32, in_place: bool = False, mutation_type: EnumMutations | None = EnumMutations.BNtoGN) -> None:
         self.numOfGroups = numOfGroups
         self.model = model
+        self.mutation_type = mutation_type
 
-    def MutateBNtoGN(self) -> nn.Module:
-        """Mutates all BatchNorm layers to GroupNorm layers in the model"""
+        # Define the mapping between EnumMutations and methods
+        self.mutation_methods = {
+            EnumMutations.BNtoGN: self.replace_batchnorm_with_groupnorm_,
+            # Add other mappings here
+        }
 
-        self.replace_batchnorm_with_groupnorm(self.model, self.numOfGroups)
+        # Run mutation in-place if required (user can then get model directly)
+        if in_place:
+            self.mutate()
+
+    def mutate(self, mutation_type: EnumMutations | None = None) -> nn.Module:
+        """
+        mutate _summary_ 
+
+        _extended_summary_
+
+        :return: _description_
+        :rtype: nn.Module
+        """        
+
+        if mutation_type is None and self.mutation_type is None:
+            raise ValueError("No mutation type provided. Please specify a mutation type.")
+        elif mutation_type is None:
+            mutation_type = self.mutation_type
+
+        # Retrieve the corresponding method based on the mutation_type
+        mutation_method = self.mutation_methods.get(mutation_type) if mutation_type is not None else None
+
+        if mutation_method is not None:
+            # Call the method with necessary arguments
+            mutation_method(self.model, self.numOfGroups)
+        else:
+            raise ValueError(
+                f"Mutation type {mutation_type} is invalid or currently not supported.")
 
         return self.model
+    
 
-    def replace_batchnorm_with_groupnorm(self, module, numOfGroups):
-        """Recursively replaces BatchNorm2d layers with GroupNorm, adjusting num_groups dynamically."""
+    def replace_batchnorm_with_groupnorm_(self, module, numOfGroups):
+        """
+        Recursively replaces BatchNorm2d layers with GroupNorm, adjusting num_groups dynamically.
+
+        :param module: The module to process.
+        :type module: nn.Module
+        :param numOfGroups: Desired number of groups for GroupNorm.
+        :type numOfGroups: int
+        """
         for name, layer in module.named_children():
             if isinstance(layer, nn.BatchNorm2d):
                 num_channels = layer.num_features
                 # Check if divisible by num_groups
                 if num_channels % self.numOfGroups != 0:
-                    num_groups = self.find_divisible_groups(num_channels)
+                    num_groups = self.find_divisible_groups_(num_channels)
                 else:
                     num_groups = self.numOfGroups
                 # Replace BN with GroupNorm
                 setattr(module, name, nn.GroupNorm(num_groups, num_channels))
             else:
                 # Recurse to find all BatchNorm layers
-                self.replace_batchnorm_with_groupnorm(layer, numOfGroups)
+                self.replace_batchnorm_with_groupnorm_(layer, numOfGroups)
 
-    def find_divisible_groups(self, num_channels):
+    def find_divisible_groups_(self, num_channels):
         """Finds an appropriate number of groups for GroupNorm that divides num_channels."""
         # Start with 32, or reduce it if it doesn’t divide num_channels
         for groups in [16, 8, 4, 2]:  # Attempt standard group sizes
