13c6ae543b0f2b5053e27be10a1eb20490b5c32c
PeterC <petercalifano.gs@gmail.com>
2025-03-29 20:53:48 +0100

    Add parser for makedoc script

--- _version.py
+++ _version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.0.dev4+gd08b6c9'
-__version_tuple__ = version_tuple = (0, 1, 0, 'dev4', 'gd08b6c9')
+__version__ = version = '0.1.0.dev7+gbfbdb29'
+__version_tuple__ = version_tuple = (0, 1, 0, 'dev7', 'gbfbdb29')
