fa8e58dc6f065ca2d009219bdcdfe6599260e8a4
PeterC <petercalifano.gs@gmail.com>
2025-04-02 16:49:42 +0200

    Update release script to fetch environment variable for token

--- release_to_pypi.sh
+++ release_to_pypi.sh
@@ -10,5 +10,5 @@ python -m build
 
 if [ -d "dist" ]; then
     twine check dist/*
-    twine upload --repository pypi dist/* -u __token__ -p pypi-AgEIcHlwaS5vcmcCJDdiNmU0ODJkLTVjNTctNDBiMi04MzU3LTA3YWY4OTVmZjdkZgACKlszLCJkMzJmOTNhNi1jMWM3LTRmYjgtYmM1OC1iODdiZDU4MzU2MzgiXQAABiAostMQFcn0HFLnuiwi3jxXFShAf_7ebZ1Hoz6YckDQiA --verbose
+    twine upload --repository pypi dist/* -u __token__ -p $PYPI_TOKEN --verbose
 fi
