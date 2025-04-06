f74329c73334a09acfb7866d8f72ae318e893838
PeterC <petercalifano.gs@gmail.com>
2025-04-06 20:58:12 +0200

    Add functionality to specify data type in conversion utility functions

--- pyTorchAutoForge/utils/conversion_utils.py
+++ pyTorchAutoForge/utils/conversion_utils.py
@@ -1,12 +1,14 @@
 from torch import Tensor, from_numpy
+import numpy as np
 from numpy import ndarray
+import torch
 
 # Interfaces between numpy and torch tensors
-def torch_to_numpy(tensor: Tensor | ndarray) -> ndarray:
+def torch_to_numpy(tensor: Tensor | ndarray, dtype : type = np.float32) -> ndarray:
 
     if isinstance(tensor, Tensor):
         # Convert to torch tensor to numpy array
-        return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+        return (tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()).astype(dtype)
 
     elif isinstance(tensor, ndarray):
         # Return the array itself
@@ -15,11 +17,11 @@ def torch_to_numpy(tensor: Tensor | ndarray) -> ndarray:
         raise ValueError("Input must be a torch.Tensor or np.ndarray")
 
 
-def numpy_to_torch(array: Tensor | ndarray) -> Tensor:
+def numpy_to_torch(array: Tensor | ndarray, dtype : torch.dtype = torch.float32) -> Tensor:
 
     if isinstance(array, ndarray):
         # Convert numpy array to torch tensor
-        return from_numpy(array)
+        return from_numpy(array).to(torch.float32)
 
     elif isinstance(array, Tensor):
         # Return the tensor itself
