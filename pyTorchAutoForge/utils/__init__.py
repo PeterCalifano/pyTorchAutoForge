c3ba711093646494723fce1105ad5633ce4e4cfc
PeterC <petercalifano.gs@gmail.com>
2025-04-18 10:20:34 +0200

    Add experimental code for context management

--- pyTorchAutoForge/utils/__init__.py
+++ pyTorchAutoForge/utils/__init__.py
@@ -1,19 +1,23 @@
-from .utils import AddZerosPadding, GetSamplesFromDataset, getNumOfTrainParams, SplitIdsArray_RandPerm, GetDevice
+from .utils import AddZerosPadding, GetSamplesFromDataset, getNumOfTrainParams, GetDevice, ComputeModelParamsStorageSize
 from .LossLandscapeVisualizer import Plot2DlossLandscape
 from .DeviceManager import GetDeviceMulti
 from .conversion_utils import torch_to_numpy, numpy_to_torch
 from .timing_utils import timeit_averaged, timeit_averaged_
+from .ArgumentParsers import PTAF_training_parser
+from .context_management import _timeout_handler, TimeoutException
 
 __all__ = [
     'GetDevice',  
     'GetDeviceMulti', 
     'Plot2DlossLandscape', 
+    'ComputeModelParamsStorageSize',
     'AddZerosPadding', 
     'GetSamplesFromDataset', 
     'getNumOfTrainParams', 
-    'SplitIdsArray_RandPerm', 
     'torch_to_numpy', 
     'numpy_to_torch', 
     'timeit_averaged', 
-    'timeit_averaged_'
+    'timeit_averaged_',
+    'PTAF_training_parser',
+    '_timeout_handler', 'TimeoutException'
     ]
