121cabb7dd59c151a7882c7f6ce5e3c03c052fa2
PeterC <petercalifano.gs@gmail.com>
2025-06-30 19:25:55 +0200

    Complete prototype implementation of ConvNeuralCOF spatial features variant

--- pyTorchAutoForge/model_building/backbones/efficient_net.py
+++ pyTorchAutoForge/model_building/backbones/efficient_net.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn.functional as F
 from pyTorchAutoForge.model_building.backbones.spatial_features_operators import SpatialKptFeatureSoftmaxLocator
 
+
 class EfficientNetBackbone(nn.Module):
     def __init__(self, cfg: EfficientNetConfig):
         super(EfficientNetBackbone, self).__init__()
@@ -43,7 +44,7 @@ class EfficientNetBackbone(nn.Module):
                     and self.cfg.feature_tapping_channel_input_size is not None:
 
                 for key, value in self.cfg.feature_tapping_output_resolution_channels.items():
-                    
+
                     if len(value) == 2:
                         target_res_key, target_channels_key = value.keys()
 
@@ -51,11 +52,14 @@ class EfficientNetBackbone(nn.Module):
                         target_res_key, target_channels_key, target_linear_output_size = value.keys()
 
                     # Get target resolution and channels from configuration
-                    target_res = self.cfg.feature_tapping_output_resolution_channels[key][target_res_key]
-                    target_channels = self.cfg.feature_tapping_output_resolution_channels[key][target_channels_key]
+                    target_res = self.cfg.feature_tapping_output_resolution_channels[
+                        key][target_res_key]
+                    target_channels = self.cfg.feature_tapping_output_resolution_channels[
+                        key][target_channels_key]
 
                     # Adaptive max pooling layer
-                    pooled_res = (4*target_res[0], 4*target_res[1]) # Pool to 4 times the target resolution
+                    # Pool to 4 times the target resolution
+                    pooled_res = (4*target_res[0], 4*target_res[1])
                     adaptive_max_pool = nn.AdaptiveMaxPool2d(pooled_res)
 
                     # Convolutional 2d extractor layer
@@ -66,41 +70,49 @@ class EfficientNetBackbone(nn.Module):
                                                          padding=1)
 
                     # Max pooling layer
-                    max_pool_extractor_out_layer = nn.AdaptiveMaxPool2d(output_size=(target_res[0], target_res[1]))
+                    max_pool_extractor_out_layer = nn.AdaptiveMaxPool2d(
+                        output_size=(target_res[0], target_res[1]))
 
                     # Activation function for output features maps
-                    features_spill_activations = nn.PReLU(num_parameters=target_channels)
+                    features_spill_activations = nn.PReLU(
+                        num_parameters=target_channels)
 
-                    if cfg.output_type == 'spill_features': # Intensity-like feature output type
+                    if cfg.output_type == 'spill_features':  # Intensity-like feature output type
                         if 'linear_output_size' not in cfg.feature_tapping_output_resolution_channels[key].keys():
                             raise ValueError(
                                 "feature_tapping_output_resolution_channels must contain 'linear_output_size' for intensity-like output type (spill_features mode).")
 
                         # Build flattening and linear layer to match expected output size
                         self.feature_spill_preprocessor[str(key)] = nn.Sequential(
-                            adaptive_max_pool,
                             max_pool_extractor_layer,
                             max_pool_extractor_out_layer,
                             features_spill_activations,
                             nn.Flatten(),
-                            nn.Linear(in_features=target_channels * target_res[0] * target_res[1], 
+                            nn.Linear(in_features=target_channels * target_res[0] * target_res[1],
                                       out_features=cfg.feature_tapping_output_resolution_channels[key][target_linear_output_size])
                         )
 
-                    else: # Spatial features output type
+                    else:  # Spatial features output type
+
+                        if target_res[0] > cfg.input_resolution[0] or target_res[1] > cfg.input_resolution[1]:
+                            raise ValueError(
+                                f"Target resolution {target_res} must not exceed input resolution {cfg.input_resolution} at stage {key}.")
+                        
+                        # Compute downsampling factor from original image resolution to target resolution
+                        downsampling_res_factor_ = (cfg.input_resolution[0] / target_res[0],
+                                                    cfg.input_resolution[1] / target_res[1])
 
                         # Build softargmax layer to extract spatial features for each channel
                         # Outputs will be of shape (B, C, 2) where 2 is for x and y coordinates, C channels
-                        spatial_kpt_extractor = SpatialKptFeatureSoftmaxLocator(target_resolution=target_res,
-                                                                                target_channels=target_channels)
+                        spatial_kpt_extractor = SpatialKptFeatureSoftmaxLocator(input_resolution=target_res,
+                                                                                num_input_channels=target_channels,
+                                                                                downsampling_res_factor=downsampling_res_factor_)
 
-                        self.feature_spill_preprocessor[str(key)] = nn.Sequential(
-                            adaptive_max_pool,
-                            max_pool_extractor_layer,
-                            max_pool_extractor_out_layer,
-                            features_spill_activations,
-                            spatial_kpt_extractor
-                        )
+                        self.feature_spill_preprocessor[str(key)] = nn.Sequential(max_pool_extractor_layer, 
+                                                                                  max_pool_extractor_out_layer,
+                                                                                  features_spill_activations,
+                                                                                  spatial_kpt_extractor
+                                                                                  )
 
             else:
                 raise ValueError(
@@ -125,24 +137,25 @@ class EfficientNetBackbone(nn.Module):
     def forward(self, x):
         features = []  # TODO this is the lazy way. It would be much faster by preallocating the list size, one way could be to run inference once and store the sizes, since these will not change at runtime.
 
-        if self.cfg.output_type not in ['last', 'spill_features']:
+        if self.cfg.output_type not in ['last', 'spill_features', 'spatial_features']:
             raise ValueError(
-                f"Invalid output_type: {self.cfg.output_type}. Must be 'last' or 'spill_features'.")
-
+                f"Invalid output_type: {self.cfg.output_type}. Must be 'last', 'spill_features', or 'spatial_features'.")
+        
         # Pass through feature extractor
         for layer in self.feature_extractor:
             x = layer(x)
 
-            if self.cfg.output_type == 'spill_features':
+            if self.cfg.output_type == 'spill_features' or self.cfg.output_type == 'spatial_features':
                 features.append(x)
 
         # Process selected features with average pooling
-        if self.cfg.output_type == 'spill_features' and self.cfg.feature_tapping_output_resolution_channels is not None:
+        if (self.cfg.output_type == 'spill_features' or self.cfg.output_type == 'spatial_features') and self.cfg.feature_tapping_output_resolution_channels is not None:
 
             for key in self.cfg.feature_tapping_output_resolution_channels.keys():
 
                 if key in self.feature_spill_preprocessor:
-                    features[int(key)] = self.feature_spill_preprocessor[key](features[int(key)])
+                    features[int(key)] = self.feature_spill_preprocessor[key](
+                        features[int(key)])
 
         # Handle output and optional head
         if self.cfg.output_type == 'last':
@@ -151,8 +164,8 @@ class EfficientNetBackbone(nn.Module):
                 out = self.output_layer(out.view(out.size(0), -1))
             return out
 
-        elif self.cfg.output_type == 'spill_features':
-            
+        elif self.cfg.output_type == 'spill_features' or self.cfg.output_type == 'spatial_features':
+
             # Append head output to the features list and return the whole list
             if self.output_layer is not None and len(features) > 0:
                 last_feat = features[-1]
@@ -162,6 +175,7 @@ class EfficientNetBackbone(nn.Module):
 
             out = features
             return out
+    
 
 
 # Define factory method for EfficientNet backbone
