5056749fc81cc521d051feabfb9db21b65f54a1d
PeterC <petercalifano.gs@gmail.com>
2025-11-27 00:05:43 +0100

    Implement prototype custom adaptive avg and max pooling 2d for onnx compatibility

--- pyTorchAutoForge/model_building/poolingBlocks.py
+++ pyTorchAutoForge/model_building/poolingBlocks.py
@@ -19,13 +19,23 @@ from pyTorchAutoForge.model_building.factories.block_factories import activ_type
 
 # TODO review and test, should be an ONNx compatible AdaptiveAvgPool2d
 class CustomAdaptiveAvgPool2d(nn.Module):
-    def __init__(self, output_size):
+    def __init__(self, output_size : tuple[int,int] | int):
         super(CustomAdaptiveAvgPool2d, self).__init__()
-        self.output_size = np.array(output_size)
+        
+        # Output_size: (H_out, W_out) or int
+        if isinstance(output_size, int):
+            output_size = (output_size, output_size)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        self.output_size = tuple(output_size)
+        if self.output_size[0] == 1 and self.output_size[1] == 1:
+            self.forward = self.forward_impl_GAP2d
+        else:
+            self.forward = self.forward_impl_adaptive2d
+
+    # Define overloads for different input dimensions
+    def forward_impl_GAP2d(self, x: torch.Tensor) -> torch.Tensor:
         """
-        forward _summary_
+        forward_impl_GAP2d _summary_
 
         _extended_summary_
 
@@ -34,27 +44,55 @@ class CustomAdaptiveAvgPool2d(nn.Module):
         :return: _description_
         :rtype: torch.Tensor
         """
-    
-        shape_x = x.shape
+        # For output size 1x1, i.e. Global Average Pooling
+        return x.mean(dim=(2, 3), keepdim=True)
 
-        if (shape_x[-1] < self.output_size[-1]):
+    def forward_impl_adaptive2d(self, x: torch.Tensor) -> torch.Tensor:
+        # shape: (N, C, H_in, W_in) -> (N, C, H_out, W_out)
+        return F.interpolate(x, size=self.output_size, mode="area")
 
-            paddzero = torch.zeros(
-                (shape_x[0], shape_x[1], shape_x[2], self.output_size[-1] - shape_x[-1]))
-            
-            paddzero = paddzero.to(x.device)
-            x = torch.cat((x, paddzero), axis=-1)
+    #def forward(self, x: torch.Tensor) -> torch.Tensor:
 
-        # Compute kernel and stride sizes to reach desired output size
-        stride_size = np.floor(
-            np.array(x.shape[-2:]) / self.output_size).astype(np.int32)
+class CustomAdaptiveMaxPool2d(nn.Module):
+    def __init__(self, output_size : tuple[int,int] | int):
+        super(CustomAdaptiveMaxPool2d, self).__init__()
+
+        # Output_size: (H_out, W_out) or int
+        if isinstance(output_size, int):
+            output_size = (output_size, output_size)
+
+        self.output_size = tuple(output_size)
+
+        if self.output_size[0] == 1 and self.output_size[1] == 1:
+            self.forward = self.forward_impl_GMP2d
+        else:
+            self.forward = self.forward_impl_adaptive2d
+
+    # Define overloads for different input dimensions
+    def forward_impl_GMP2d(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        forward_impl_GMP2d _summary_
+
+        _extended_summary_
+
+        :param x: _description_
+        :type x: torch.Tensor
+        :return: _description_
+        :rtype: torch.Tensor
+        """
+        # For output size 1x1, i.e. Global Max Pooling
+        return x.max(dim=(2, 3), keepdim=True)[0]
+
+    def forward_impl_adaptive2d(self, x: torch.Tensor) -> torch.Tensor:
         
-        kernel_size = np.array(x.shape[-2:]) - \
-            (self.output_size - 1) * stride_size
-        
-        avg = nn.AvgPool2d(kernel_size=list(kernel_size),
-                           stride=list(stride_size))
-        
-        x = avg(x)
+        # Shape: (N, C, H_in, W_in) -> (N, C, H_out, W_out)
+        H_out, W_out = self.output_size
 
-        return x
+        B, C, H, W = x.shape
+        if H % H_out != 0 or W % W_out != 0:
+            raise ValueError(
+                f"ONNX-safe adaptive max pool needs input divisible by output; "
+                f"got H={H}, W={W}, target={self.output_size}"
+            )
+        x = x.reshape(B, C, H_out, H // H_out, W_out, W // W_out)
+        return x.amax(dim=(3, 5))
